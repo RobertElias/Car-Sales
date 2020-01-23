0cf13982661cd47b5d33aebfcff28c882f193455
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-23 12:30:15 -0800

    updated mapToState on App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
+import {addFeature, removeFeature, updateTotal} from './actions'
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
@@ -38,15 +39,23 @@ const App = (props) => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={props.car}additionalFeatures={props.additionalFeatures}/>
+        <AddedFeatures car={props.car} removeFeature={removeFeature}/>
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={props.store} addFeature={buyItem} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    car: state.car,
+    store: state.store,
+    additionalPrice: state.additionalPrice
+  }
+}
+
+export default connect(mapStateToProps, { addFeature, removeFeature, updateTotal })(App);
