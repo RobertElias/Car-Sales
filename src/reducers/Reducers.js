94a77b88007e0f61ef7175d87730f901a20a81a1
Robert Elias <RobertElias@users.noreply.github.com>
2020-01-23 12:44:48 -0800

    updates to AdditionalFeature.js

--- src/reducers/Reducers.js
+++ src/reducers/Reducers.js
@@ -1,50 +1,55 @@
-import {AddFeature, RemoveFeature, UpdateTotal, addFeature} from '../actions';
+import { AddFeature, RemoveFeature, UpdateTotal } from "../actions";
 
 export const initialState = {
   additionalPrice: 0,
   car: {
     price: 26395,
-    name: '2019 Ford Mustang',
+    name: "2019 Ford Mustang",
     image:
-      'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+      "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
     features: []
   },
   store: [
-    { id: 1, name: 'V-6 engine', price: 1500 },
-    { id: 2, name: 'Racing detail package', price: 1500 },
-    { id: 3, name: 'Premium sound system', price: 500 },
-    { id: 4, name: 'Rear spoiler', price: 250 }
+    { id: 1, name: "V-6 engine", price: 1500 },
+    { id: 2, name: "Racing detail package", price: 1500 },
+    { id: 3, name: "Premium sound system", price: 500 },
+    { id: 4, name: "Rear spoiler", price: 250 }
   ]
 };
 
-export const Reducer = ( state = initialState, action ) =>{
-    console.log(action, "Consolelogging action");
+export const Reducer = (state = initialState, action) => {
+  console.log(action, "Consolelogging action");
 
-    switch(action.type) {
-      // adds features to cart
-      case addFeature: return {
+  switch (action.type) {
+    // adds features to cart
+    case AddFeature:
+      return {
         ...state,
         car: {
           ...state.car,
           features: [...state.car.features, action.payload]
         },
         store: state.store.filter(addOn => addOn.id !== action.payload.id)
-      }
-      // removes features from cart
-      case RemoveFeature: return {
+      };
+    // removes features from cart
+    case RemoveFeature:
+      return {
         ...state,
         car: {
           ...state.car,
-          features: state.car.features.filter(addOn => addOn.id !== action.payload.id)
+          features: state.car.features.filter(
+            addOn => addOn.id !== action.payload.id
+          )
         },
         store: [...state.store, action.payload]
-      }
-      // updates total on cart
-      case UpdateTotal: return {
+      };
+    // updates total on cart
+    case UpdateTotal:
+      return {
         ...state,
         additionalPrice: state.additionalPrice + action.payload
-      }
-      default: return state;
-    }
+      };
+    default:
+      return state;
   }
-
+};
